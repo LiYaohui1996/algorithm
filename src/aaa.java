636d01e9f4f81fe8fb110f20095ca8f393f9505e
LiYaohui1996 <lyh960509@163.com>
2018-08-23 17:05:05 +0800

    提交了算法基础

---
+++ src/aaa.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: 李耀辉
+ * @Date: 2018/8/23 16:58
+ * @Description:
+ */
+public class aaa {
+    public static void main(String args[]){
+        System.out.println("aaaa");
+    }
+}
